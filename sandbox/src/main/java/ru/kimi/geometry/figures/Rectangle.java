6232783c1596fee3287d69a8fc3511a3ca21f521
Kimi <jakeathene@gmail.com>
2024-02-21 12:58:05 +0300

    Выполнена структуризация кода (Урок 1.6)

--- sandbox/src/main/java/ru/kimi/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/kimi/geometry/figures/Rectangle.java
@@ -2,7 +2,8 @@ package ru.kimi.geometry.figures;
 
 public class Rectangle {
     public static void printRectangleArea(double a, double b) {
-        System.out.println("Площадь прямоугольника со стороной " + a + " и " + b + " = " + rectangleArea(a, b));
+        String text = String.format("Площадь прямоугольника со стороной %f и %f = %f",a, b, rectangleArea(a, b));
+        System.out.println(text);
     }
 
     private static double rectangleArea(double a, double b) {
--- sandbox/src/main/java/ru/kimi/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/kimi/geometry/figures/Square.java
@@ -2,7 +2,8 @@ package ru.kimi.geometry.figures;
 
 public class Square {
     static public void printSquareArea(double side) {
-        System.out.println("Площадь квадрата со стороной " + side + " = " + squareArea(side));
+        String text = String.format("Площадь квадрата со стороной %f = %f", side, squareArea(side));
+        System.out.println(text);
     }
 
     private static double squareArea(double a) {
